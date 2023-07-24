2ffe69f5f1e08391275879501ef122582b837408
RamonSouto <rdssi@hotmail.com>
2023-07-24 20:48:31 -0300

    test the reactivity composition-api

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,36 @@
 <template>
+  {{ count }}
+  {{ userName }}
+
+  <ul>
+    <li v-for="user in users">{{user.firstName }} - {{ user.age }}</li>
+  </ul>
+  <button @click="count++">Add</button>
   <h2>App</h2>
 
   <router-link to="/">Home</router-link>
   <router-link to="/about">About</router-link>
   <router-view />
 
-  <p>{{ count }}</p>
-
-  <button @click="count++">Add</button>
+  
 </template>
 
 <script setup>
-  import { onMounted, onUpdated, ref } from 'vue';
+  import { onMounted, onUpdated, reactive, ref } from 'vue';
 
   const count = ref(0);
+  const userName = ref('Ramon')
+
+  const users = reactive([
+    {
+      firstName: "Ramon",
+      age: 36
+    },
+    {
+      firstName: "Dani",
+      age: 39
+    }
+  ])
 
   onMounted(() => {
     console.log('Montado App')
