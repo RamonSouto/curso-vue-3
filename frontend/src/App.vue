3b186950816b327c2bc537ecbb00b9119cb3e68a
RamonSouto <rdssi@hotmail.com>
2023-07-23 22:34:12 -0300

    test the momounted unmounted e update composition-api

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,85 +1,28 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <h2>App</h2>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <router-link to="/">Home</router-link>
+  <router-link to="/about">About</router-link>
+  <router-view />
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+  <p>{{ count }}</p>
 
-  <RouterView />
+  <button @click="count++">Add</button>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<script setup>
+  import { onMounted, onUpdated, ref } from 'vue';
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+  const count = ref(0);
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
+  onMounted(() => {
+    console.log('Montado App')
+  })
+  onUpdated(() => {
+    console.log('Update App')
+  })
+</script>
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
+<style>
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About</h1>
   </div>
 </template>
+
+<script setup>
+  import { onUnmounted } from 'vue';
+
+  onUnmounted(() => {
+    console.log('Desmontado about')
+  })
+</script>
 
 <style>
 @media (min-width: 1024px) {
