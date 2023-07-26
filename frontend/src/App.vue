47056d27f09b6ed107152eea60a9c5e2a9d03bb9
RamonSouto <rdssi@hotmail.com>
2023-07-25 21:01:53 -0300

    conditional v-If and v-Else

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,23 +1,38 @@
 <template>
+  
+
+    <template v-if="showHeader">
+      <Header />
+    </template>
+
+  <hr>
   <h2>Lista de nome</h2>
   <ul>
-    <template v-for="(user, key) in users" :key="key">
-      <li>{{user.id}} - {{ user.firstName }} {{ user.lastName }} - {{ user.email }}</li>
+    <template v-for="(user) in users" :key="user.id">
+      <li>
+        {{user.id}} - {{ user.firstName }} {{ user.lastName }} - {{ user.email }}
+        <button v-if="user.is_admin ===1">Admin</button>
+        <button v-else>Not Admin</button>
+      </li>
     </template>
   </ul>
 </template>
 
 <script>
   import http from '@/services/http.js'
-
+  import Header from '@/components/Header.vue'
   export default{
     
     data() {
       return {
         users: [],
+        showHeader: false
       }
     },
 
+    components:{
+      Header
+    },
     async mounted(){
       try {
         const {data} = await http.get('http://localhost/api/users')
